c47e002fee963cc75549422185703d06f9f152ee
enhuijiang <68130850+enhuijiang@users.noreply.github.com>
2021-03-29 20:09:41 -0400

    Update drop.js

--- lab4-dragAndDrop/drop.js
+++ lab4-dragAndDrop/drop.js
@@ -15,7 +15,7 @@ var game = new Phaser.Game(config);
 
 function preload ()
 {
-    this.load.image('image','images.png');
+    this.load.image('image','assets/images.png');
 }
 
 function create ()
